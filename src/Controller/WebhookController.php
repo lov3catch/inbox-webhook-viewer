f9b3d37630c01029dcbb65db561f2fdd5a81a6c5
Ihor K <lov3catch@gmail.com>
2021-02-22 18:29:43 +0200

    - add verify route

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -9,16 +9,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 
 class WebhookController extends AbstractController
 {
-    /**
-     * @Route("/instagram", name="instagram")
-     */
     public function instagram(Request $request): Response
     {
-        return new Response('OK');
+        if ($request->isMethod('POST')) {
+            return new Response('OK');
+        }
+
+        $challenge = $request->request->get('hub_challenge');
+        $verifyToken = $request->request->get('hub_verify_token');
+
+        return new Response($challenge);
     }
 }
